dc7106b7623878c65a4c5c7774fa1d3cf2b7b8a6
Federico Carbone <federico.carbone20@gmail.com>
2024-08-31 16:15:40 +0200

    chore: updated to v3.1.0

--- Core/Inc/conf.h
+++ Core/Inc/conf.h
@@ -23,7 +23,7 @@
 
 /* ---------- Exported constants --------------------------------------------*/
 #define VERSION_MAJOR 3
-#define VERSION_MINOR 0
+#define VERSION_MINOR 1
 #define VERSION_PATCH 0
 
 #define DEBUG_SERIAL
